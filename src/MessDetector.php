dd08652cc2251fee40b5ad6474b78bd6e366039c
johannes.pichler <johannes.pichler@karriere.at>
2016-08-09 07:55:48 +0200

    add phpmd

---
+++ src/MessDetector.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Karriere\CodeQuality;
+
+use Composer\Script\Event;
+use Karriere\CodeQuality\Console\ScriptArgumentsTrait;
+use Karriere\CodeQuality\Process\Process;
+
+class MessDetector implements ComposerScriptInterface
+{
+    use ScriptArgumentsTrait;
+
+    private static $commands = [
+        'local' => 'phpmd src text ' . __DIR__ . '/../config/phpmd.xml',
+        'jenkins' => 'phpmd src xml ' . __DIR__ . '/../config/phpmd.xml --reportfile phpmd.xml'
+    ];
+
+    public static function run(Event $event)
+    {
+        $eventArguments = self::getComposerScriptArguments($event->getArguments());
+
+        $command = self::getArrayValueByEventArguments(self::$commands, $eventArguments, 'env');
+
+        $composerIO = $event->getIO();
+        $composerIO->write('<info>Running </info><fg=green;options=bold>' . $command . '</>');
+
+        $process = new Process($command);
+        $process->setTtyByArguments($eventArguments);
+        $process->run();
+
+        return $process->getExitCode();
+    }
+}
